a1059c613a9fd56651a210cf4c86e7a88dbc5d38
Sebastian Sauerbier <s.sauerbier@mid.de>
2020-08-12 22:00:57 +0200

    Disable error-controller.

--- pdf-creation-service/src/main/java/de/woody64k/services/pdf/config/SwaggerConfig.java
+++ pdf-creation-service/src/main/java/de/woody64k/services/pdf/config/SwaggerConfig.java
@@ -2,10 +2,12 @@ package de.woody64k.services.pdf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,14 +21,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	/**
-	 * Schaltet die Swagger-UI ein.
-	 * 
-	 * @return
-	 */
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("de.woody64k.services.pdf")).paths(PathSelectors.any())
-				.build();
-	}
+    /**
+     * Schaltet die Swagger-UI ein.
+     * 
+     * @return
+     */
+    @Bean
+    public Docket api() {
+	return new Docket(DocumentationType.SWAGGER_2).select()
+		.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+		.build().apiInfo(metadata());
+    }
+
+    private ApiInfo metadata() {
+	return new ApiInfoBuilder().title("PDF-Creation-Service")
+		.description("A service to generate an PDF from inputdata").build();
+    }
 }
